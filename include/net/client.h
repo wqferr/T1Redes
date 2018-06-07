49ad7011611936eb2de5a1a31a6e03fdbebb1ad2
wqferr <wqferr@gmail.com>
2018-06-07 16:01:04 -0300

    Implement send and recv

--- include/net/client.h
+++ include/net/client.h
@@ -1,10 +1,12 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
+#include <stddef.h>
 #include <sys/socket.h>
 
 #define ERR_CLIENT_CREATE_SOCKET 1
 #define ERR_CLIENT_CONNECT_SOCKET 2
+#define ERR_CLIENT_SOCKET_CLOSED 3
 
 
 typedef struct client client;
@@ -16,6 +18,7 @@ int client_create(
         int socktype);
 int client_close(client *cl);
 
-int client_send(client *cl, void *msg, int msglen);
+int client_send(client *cl, const void *msg, size_t msglen);
+int client_recv(client *cl, void *buf, size_t bufsize, size_t *nread);
 
 #endif
--- src/net/client.c
+++ src/net/client.c
@@ -37,3 +37,17 @@ int client_close(client *cl) {
     free(cl);
     return 0;
 }
+
+
+int client_send(client *cl, const void *msg, size_t msglen) {
+    write(cl->sockfd, msg, msglen);
+    return 0;
+}
+
+int client_recv(client *cl, void *buf, size_t bufsize, size_t *nread) {
+    *nread = read(cl->sockfd, buf, bufsize);
+    if (*nread < 0) {
+        return ERR_CLIENT_SOCKET_CLOSED;
+    }
+    return 0;
+}
