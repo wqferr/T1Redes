f4212545a4b4f3ac112af0d9b12a9e32a60452b6
wqferr <wqferr@gmail.com>
2018-06-07 15:27:25 -0300

    Add init and deinit interfaces for server

--- include/net/server.h
+++ include/net/server.h
@@ -1,4 +1,18 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#include <sys/socket.h>
+
+#define ERR_SERVER_CREATE_SOCKET 1
+#define ERR_SERVER_BIND_SOCKET 2
+
+typedef struct server server;
+
+int server_create(
+        server **sv,
+        struct sockaddr *serveraddr,
+        socklen_t addrlen,
+        int socktype);
+int server_close(server *sv);
+
 #endif
