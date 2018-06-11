12cb1d813d12dae6fa7504a0c4639a3e0252caf1
wqferr <wqferr@gmail.com>
2018-06-10 22:24:13 -0300

    Comment client.c and server.c

--- src/net/server.c
+++ src/net/server.c
@@ -16,20 +16,24 @@ int server_create(
         struct sockaddr *serveraddr,
         socklen_t addrlen,
         int socktype) {
+    int enable;
+    
     server *sv = malloc(sizeof(*sv));
     sv->clients = NULL;
     sv->nclients = 0;
 
     sv->sockfd = socket(AF_INET, socktype, 0);
     if (sv->sockfd == -1) {
+        // Couldn't create socket
         free(sv);
         return ERR_SERVER_CREATE_SOCKET;
     }
 
-    int enable = 1;
+    enable = 1;
     setsockopt(sv->sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
 
     if (bind(sv->sockfd, serveraddr, addrlen) < 0) {
+        // Couldn't create server
         close(sv->sockfd);
         free(sv);
         return ERR_SERVER_BIND_SOCKET;
@@ -44,6 +48,7 @@ int server_close(server *sv) {
     int i;
     close(sv->sockfd);
     for (i = 0; i < sv->nclients; i++) {
+        // Close connection to all clients
         close(sv->clients[i]);
     }
     free(sv->clients);
@@ -56,11 +61,14 @@ int server_awaitClients(server *sv, int n) {
     int client;
 
     if (sv->clients != NULL || listen(sv->sockfd, n) < 0) {
+        // Server is already listening, has clients connected
+        // or listen returned an error
         return ERR_SERVER_LISTEN;
     }
     sv->nclients = 0;
     sv->clients = malloc(n * sizeof(*sv->clients));
     while (sv->nclients < n) {
+        // Loop until n clients are successfully accepted
         client = accept(sv->sockfd, NULL, 0);
         if (client >= 0) {
             sv->clients[sv->nclients] = client;
@@ -73,9 +81,11 @@ int server_awaitClients(server *sv, int n) {
 
 int server_send(server *sv, int clientid, const void *msg, size_t msglen) {
     if (clientid < 0 || clientid >= sv->nclients) {
+        // Client index out of bounds
         return ERR_SERVER_NO_SUCH_CLIENT;
     }
     if (write(sv->clients[clientid], msg, msglen) < 0) {
+        // Error sending message
         return ERR_SERVER_SOCKET_CLOSED;
     }
     return 0;
@@ -83,10 +93,12 @@ int server_send(server *sv, int clientid, const void *msg, size_t msglen) {
 
 int server_recv(server *sv, int clientid, void *buf, size_t bufsize, size_t *nread) {
     if (clientid < 0 || clientid >= sv->nclients) {
+        // Client index out of bounds
         return ERR_SERVER_NO_SUCH_CLIENT;
     }
     *nread = read(sv->clients[clientid], buf, bufsize);
     if (*nread < 0) {
+        // Error reading message
         return ERR_SERVER_SOCKET_CLOSED;
     }
     return 0;
